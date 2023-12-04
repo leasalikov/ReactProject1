4821a8a257fc594cd20d862b4730aac3b3d500c2
leasalikov <Hand4c18@mbjcomp.org.il>
2023-12-04 14:42:05 +0200

    start

---
+++ reactProject2/src/components/LogIn.jsx
@@ -0,0 +1,20 @@
+
+import { useState } from "react";
+
+function LogIn(){
+
+const signUser=()=>{
+
+}
+
+return(
+<div>
+    <h1>Get to 100!!!</h1>
+    <h2>Enter the player's name:</h2>
+    <input></input>
+    <button onClick={signUser()}>👍</button>
+</div>
+)   
+
+}
+export default LogIn;
