cfaebcbf269fcf7207eb10073c4e56c2c2391720
leasalikov <Hand4c18@mbjcomp.org.il>
2023-12-04 14:04:43 +0200

    תיקון UNDO

--- reactProject1/src/assets/components/SpecialKeys.jsx
+++ reactProject1/src/assets/components/SpecialKeys.jsx
@@ -8,37 +8,28 @@ function SpecialKeys(prop){
         prop.setText([]);
     }
 
-    const writeAll=(all)=>{
-        prop.setText(all);
-    }
+    // const writeAll=(all)=>{
+    //     prop.setText(all);
+    // }
 
     const CapsUpAll = () => {
-        let newText = prop.text;
-        newText.map((myletter) => (myletter.letter.toUpperCase()));
-        // prop.setText((prop.text).map((myletter) => (myletter.letter.toUpperCase())));
+        let newText = prop.text.map((myletter) => ({
+            ...myletter,
+            letter: myletter.letter.toUpperCase()
+        }));
         prop.setText(newText);
-    }
-    // const toUpperCase=()=>{
-        
-    // }
+    };
 
-    // const undoButton=()=>{
-    //     prop.undo[prop.undo.length-1].func(prop.undo[prop.undo.length-1].arg);
-    //     let prevUndo = prop.undo.slice(0, -1);
-    //     prop.setUndo(prevUndo);
-    //     console.log(prop.undo);
-    // }
-    const UndoButton=()=>{
-        console.log(prop.undo[prop.undo.length-1]);
-        if(prop.undo[prop.undo.length-1]=={color: 'black'})
-            prop.setColor({color: 'black'});
-        else
-            prop.setText(prop.undo[prop.undo.length-1]);
-        let prevUndo = prop.undo;
-        prevUndo=prevUndo.slice(0, -1);
+    const UndoButton = () => {
+        console.log(prop.undo[prop.undo.length - 1]);
+        if (prop.undo[prop.undo.length - 1] && prop.undo[prop.undo.length - 1].color === 'black') {
+            prop.setColor({ color: 'black' });
+        } else if (prop.undo[prop.undo.length - 1]) {
+            prop.setText(prop.undo[prop.undo.length - 1]);
+        }
+        let prevUndo = prop.undo.slice(0, -1);
         prop.setUndo(prevUndo);
-        
-    }
+    };
     return(
         <>
             <button onClick={() => CapsUpAll()}>UPPER ALL</button>
