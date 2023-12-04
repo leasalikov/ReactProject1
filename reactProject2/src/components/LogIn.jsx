44338e28c899a0a8cc87e2577bf23cac25a3a8e4
קופינסקי דבורה אסתר <  @B18>
2023-12-04 21:36:11 +0200

    !!!!

--- reactProject2/src/components/LogIn.jsx
+++ reactProject2/src/components/LogIn.jsx
@@ -1,20 +1,36 @@
 
 import { useState } from "react";
+// import { useRef } from "react";
+
 
 function LogIn(){
 
-const signUser=()=>{
-
-}
+    const [players, setPlayers] = useState([{name:""}]);
+    const signUser = () =>{
+     let gamer= prompt("enter name:");
+     console.log(players.filter(obj=>obj.name===gamer));
+// if(players.)
+    if(players.filter(obj=>obj.name===gamer)=="[]"){
+        console.log("h")
+        setPlayers(prevPlayers => [...prevPlayers, {name:gamer}]);
+      }
+    //  if(players.map((player, i) =>{key={i} (player===gamer)?true:false}))
+        
+    }
 
 return(
-<div>
+<>
     <h1>Get to 100!!!</h1>
     <h2>Enter the player's name:</h2>
-    <input></input>
-    <button onClick={signUser()}>👍</button>
-</div>
+    <button onClick = {()=>{signUser()}} >😭😭😭😭😭😭😭😭😭</button>
+    <br/>
+    {players.map((player, i) =>
+            <>
+            <br/>
+            <span key={i} >{player.name}</span>
+            </>
+        )}
+</>
 )   
-
 }
 export default LogIn;
