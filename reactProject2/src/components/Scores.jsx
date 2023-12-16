7f6169d6c0bbb0c3770c377abe5ba451036e8465
דבורה אסתר קופינסקי <Hand4c27@mbjcomp.org.il>
2023-12-16 22:08:40 +0200

    scores

--- reactProject2/src/components/Scores.jsx
+++ reactProject2/src/components/Scores.jsx
@@ -1,25 +1,24 @@
 
-function Scores(props){
-    const{player, players} = props;
+function Scores(props) {
+    const { player, players } = props;
     const playersArray = players;
-    let myLocalStoragePlayer;
-    if(player.active=="Win"){
+    if (player.active == "Win") {
         let localStoragePlayers = JSON.parse(localStorage.getItem('players'));
-        playersArray[playersArray.indexOf(player)].scores.push(player.steps);
-        // playersArray.every(obj => {obj.name !== player.name})
-        myLocalStoragePlayer=localStoragePlayers.forEach((localStoragePlayer) => {
-            if(localStoragePlayer.name==localStoragePlayer.name){
-                localStoragePlayers[localStoragePlayers.indexOf(localStoragePlayer)].scores.push(player.steps);
-                localStorage.setItem('players', JSON.stringify(localStoragePlayers));
-                console.log(localStoragePlayers);
-            }})
-        
-        // localStoragePlayers[localStoragePlayers.indexOf(myLocalStoragePlayer)].scores.push(player.steps);
-        // localStorage.setItem('players', JSON.stringify(localStoragePlayers));
-                // console.log(player.localStoragePlayers);
+        console.log(playersArray[playersArray.indexOf(player)]);
+        if (playersArray[playersArray.indexOf(player)].lastScore !== player.steps) {
+            console.log(playersArray[playersArray.indexOf(player)]);
+            playersArray[playersArray.indexOf(player)].lastScore = player.steps;
+            playersArray[playersArray.indexOf(player)].scores.push(player.steps);
+            localStoragePlayers.forEach((localStoragePlayer) => {
+                if (localStoragePlayer.name == player.name) {
+                    localStoragePlayers[localStoragePlayers.indexOf(localStoragePlayer)].scores.push(player.steps);
+                    localStorage.setItem('players', JSON.stringify(localStoragePlayers));
+                }
+            })
+        }
     }
-return(
-    <></>
-);
+    return (
+        <></>
+    );
 }
 export default Scores;
